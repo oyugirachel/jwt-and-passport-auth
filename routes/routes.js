3ef60410fc69417af84027290f5d13609d7cf19a
oyugirachel <oyugirachel20@gmail.com>
2022-01-06 13:11:52 +0300

    Creating the Login Endpoint and Signing the JWT

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const passport = require('passport');
+const jwt = require('jsonwebtoken');
 
 const router = express.Router();
 
@@ -15,6 +16,40 @@ router.post(
             message: 'Signup successful',
             user: req.user
         });
+    }
+);
+
+// Handling of a POST request for login
+
+router.post(
+    '/login',
+    async(req, res, next) => {
+        passport.authenticate(
+            'login',
+            async(err, user, info) => {
+                try {
+                    if (err || !user) {
+                        const error = new Error('An error occurred.');
+
+                        return next(error);
+                    }
+
+                    req.login(
+                        user, { session: false },
+                        async(error) => {
+                            if (error) return next(error);
+
+                            const body = { _id: user._id, email: user.email };
+                            const token = jwt.sign({ user: body }, 'TOP_SECRET');
+
+                            return res.json({ token });
+                        }
+                    );
+                } catch (error) {
+                    return next(error);
+                }
+            }
+        )(req, res, next);
     }
 );
 
